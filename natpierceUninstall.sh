6813250dbef59a38b0e1d13244af84ed00c7c55b
s6tiger <s6tiger@163.com>
2024-10-09 23:23:17 +0700

    Add files via upload
    updated done and added blank lines

--- natpierceUninstall.sh
+++ natpierceUninstall.sh
@@ -3,7 +3,11 @@
 echo "Stopping and disabling Service"
 sudo systemctl stop natpierce.service
 sudo systemctl disable natpierce.service
+
 echo "Removing natpierce.service"
 sudo rm -rf /etc/systemd/system/natpierce.service
+
 echo "Removing /home/deck/.natpierce"
 sudo rm -rf /home/deck/.natpierce
+
+echo "Done!"
